ec088b559877c2ff1799184ea6f344bd38ec4a21
esthernguyen2701 <dp1.2017.nguyenuyennhi99@gmail.com>
2019-12-12 20:57:44 +0700

    show tên CN + tên của NV

---
+++ BUS/NhanVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAO;
+namespace BUS
+{
+    public class NhanVien
+    {
+        public static DTO.NhanVien GetNhanVien(string userName, string passWord)
+        {
+            return DAO.NhanVien.GetNhanVien(userName, passWord);
+        }
+    }
+}
--- DAO/NhanVien.cs
+++ DAO/NhanVien.cs
@@ -8,46 +8,29 @@ using System.Threading.Tasks;
 using DTO;
 namespace DAO
 {
-    public class ThanhVien
+    public class NhanVien
     {
-        public static DTO.ThanhVien GetThanhVien(string userName, string passWord)
+        public static DTO.NhanVien GetNhanVien(string userName, string passWord)
         {
             SqlConnection con = DataProvider.GetConnection();
-            SqlCommand cmd = new SqlCommand("sp_GetThanhVien", con);
+            SqlCommand cmd = new SqlCommand("sp_GetNhanVien", con);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add("@UserName", SqlDbType.NVarChar, 100).Value = userName;
             cmd.Parameters.Add("@PassWord", SqlDbType.NVarChar, 500).Value = passWord;
 
             SqlDataReader reader = cmd.ExecuteReader();
-            DTO.ThanhVien result = null;
+            DTO.NhanVien result = null;
 
             while (reader.Read())
             {
-                result = new DTO.ThanhVien();
-                result.MaThanhVien = int.Parse(reader["MATHANHVIEN"].ToString());
+                result = new DTO.NhanVien();
+                result.MaNhanVien = int.Parse(reader["MANHANVIEN"].ToString());
                 result.HoTen = reader["HOTEN"].ToString();
                 result.Cmnd = reader["CMND"].ToString();
-                result.NgaySinh = reader["NGAYSINH"].ToString();
                 result.MaChiNhanh = int.Parse(reader["MACHINHANH"].ToString());
                 result.Sdt = reader["SDT"].ToString();
-                result.Email = reader["EMAIL"].ToString();
-                try
-                {
-                   result.DiemTichLuy = double.Parse(reader["DIEMTICHLUY"].ToString());
-                }
-                catch
-                {
-                    result.DiemTichLuy = 0;
-                }
+                result.Email = reader["EMAIL"].ToString();               
                 result.MatKhau = reader["MATKHAU"].ToString();
-                if (reader["ISDELETED"].ToString().Equals("true"))
-                {
-                    result.IsDeleted = true;
-                } else
-                {
-                    result.IsDeleted = false;
-                }
-                break;
             }
             DataProvider.CloseConnection(con);
             return result;
